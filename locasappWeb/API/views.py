4af54c5ee734ae03a8beca85dfa5ff5f04cbe897
caille_j <caille_j@epitech.eu>
2015-09-15 10:33:08 +0000

    Basis of home doc page

--- locasappWeb/API/views.py
+++ locasappWeb/API/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class docAPIView(TemplateView):
+	template_name = "API/homeDocAPI.html"
