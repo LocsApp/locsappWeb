ea1490abf6731fcabd4816200d5f75c16aaebb45
caille_j <caille_j@epitech.eu>
2015-09-16 17:25:22 +0000

    added connection to mongodb

--- locasappWeb/API/views.py
+++ locasappWeb/API/views.py
@@ -1,7 +1,13 @@
+#Django imports
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.http import JsonResponse
 from django.http import HttpResponse
+
+#Pymongo imports
+from pymongo import MongoClient
+
+mongodb_client = MongoClient('localhost', 27017)
 
 # Template for the doc on the homepage of the API
 class docAPIView(TemplateView):
