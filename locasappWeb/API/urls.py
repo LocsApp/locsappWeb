f689915bde4715a37cc589efd25042667fb69198
caille_j <caille_j@epitech.eu>
2015-09-16 16:22:04 +0000

    fixed the routes and began the getuserbyid

--- locasappWeb/API/urls.py
+++ locasappWeb/API/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
+from . import views
 
+# End points for the user
 user_patterns = [
-	#url(r'^$', ),
+	url(r'^id/(?P<id>[0-9]+)$', views.getUserById),
 ]
 
+# General urls for the api
 api_patterns = [
-	url(r'^users/$', include(user_patterns)),
+	url(r'^users/', include(user_patterns)),
 ]
