2c20d010da47f65bfca96d44406faac995a2cb2a
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-09-24 17:54:24 +0200

    add csrf in angular

--- locsAppFrontWeb/dev/static/js/locsApp.src.js
+++ locsAppFrontWeb/dev/static/js/locsApp.src.js
@@ -11,10 +11,21 @@
             //'locsAppConfig',
             NAME_PROJECT + 'Routes',
             NAME_PROJECT + 'User',
-        ]);
+        ])
+
+        .run(run);
+
+    run.$inject = ['$http', '$rootScope'];
+
+    function run($http) {
+
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+        
+    };
 
 
     angular.module(NAME_PROJECT + 'Routes', ['ngRoute']);
-  //  angular.module('locsAppConfig', []);
+    //  angular.module('locsAppConfig', []);
 
 })();
