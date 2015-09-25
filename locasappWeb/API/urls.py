40ea21200b6dde9670411748e69587c5e9f608aa
caille_j <caille_j@epitech.eu>
2015-09-25 15:41:43 +0000

    implementation of rest-auth and allauth

--- locasappWeb/API/urls.py
+++ locasappWeb/API/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import include, url
+# django_rest_framework
+from rest_framework.authtoken.views import obtain_auth_token
+# importing local views
 from . import views
 
-# End points for the user
-user_patterns = [
-	url(r'^id/(?P<id>[0-9]+)$', views.getUserById),
-	url(r'^subscribe$', views.postSubscribeNewUser),
-	url(r'^delete$', views.postDeleteUserById),
+article_patterns = [
+	url(r'^create/$', views.postNewArticle),
 ]
 
 # General urls for the api
 api_patterns = [
-	url(r'^users/', include(user_patterns)),
+	#url(r'^users/', include(user_patterns)),
+	url(r'^articles/', include(article_patterns)),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
