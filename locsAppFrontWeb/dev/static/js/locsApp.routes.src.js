a1d88d50ee819484f15a0d2d498b99831369255f
caille_j <caille_j@epitech.eu>
2015-10-04 14:48:23 +0000

    removed the # and switch to html5mode

--- locsAppFrontWeb/dev/static/js/locsApp.routes.src.js
+++ locsAppFrontWeb/dev/static/js/locsApp.routes.src.js
@@ -9,15 +9,19 @@
     /* Module creation */
     angular.module(NAME_PROJECT + 'Routes', ['ngRoute', 'ui.router']);
 
-    /* Instanciation of module  NAME_PROJECT + 'Routes'. Binding the config function to the module*/
+    /* Instanciation of module  NAME_PROJECT + 'Routes'. Binding the config  ui-router function to the module */
     angular.module(NAME_PROJECT + 'Routes')
-        .config(config);
+        .config(configUirouter);
+
+    /* Instanciation of module  NAME_PROJECT + 'Routes'. Binding the config for html5mode function to the module */
+    angular.module(NAME_PROJECT + 'Routes')
+        .config(configHTML5mode);
 
     /* Injection of the needed vars for ui-router */
-    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    configUirouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     /* Configuration of ui-router */
-    function config($stateProvider, $urlRouterProvider) {
+    function configUirouter($stateProvider, $urlRouterProvider) {
         //Redirects to /home otherwise
         $urlRouterProvider.otherwise('/home');
 
@@ -29,5 +33,12 @@
         })
     }
 
+    /* Injection of the needed vars for html5mode */
+    configHTML5mode.$inject = ['$locationProvider'];
+
+    /* Configuration of html5mode */
+    function configHTML5mode($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
 
 })();
