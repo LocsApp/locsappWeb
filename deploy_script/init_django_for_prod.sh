9d3a760a697207cb653e7318631f901251935182
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-06 17:17:51 +0200

    change script prod

--- deploy_script/init_django_for_prod.sh
+++ deploy_script/init_django_for_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source /home/sylflo/.virtualenvs/locsAppBack/bin/activate
-pip install -r /var/www/locsapp.sylflo.fr/locsappWeblocsAppBack/requirements.txt
+pip install -r /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/requirements.txt
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py collectstatic --noinput
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py makemigrations
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py migrate
