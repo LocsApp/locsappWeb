d99fd3a88103df10909e2c9e80ba875dff8d38f7
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-06 14:14:51 +0200

    call to gulp for prod

--- deploy_script/init_django_for_prod.sh
+++ deploy_script/init_django_for_prod.sh
@@ -5,4 +5,5 @@ pip install -r /var/www/locsappWeb/locsAppBack/requirements.txt
 python  /var/www/locsappWeb/locsAppBack/manage.py collectstatic --noinput
 python  /var/www/locsappWeb/locsAppBack/manage.py makemigrations
 python  /var/www/locsappWeb/locsAppBack/manage.py migrate
+/var/www/locsappWeb/locsAppFrontWeb/dev/launch_all_gulp.sh
 
