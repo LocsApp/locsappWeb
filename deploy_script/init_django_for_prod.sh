0e2e4acb26d4785cd488626a0b116fd3cd88f1b2
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-06 16:09:02 +0200

    add npm install;

--- deploy_script/init_django_for_prod.sh
+++ deploy_script/init_django_for_prod.sh
@@ -5,5 +5,7 @@ pip install -r /var/www/locsappWeb/locsAppBack/requirements.txt
 python  /var/www/locsappWeb/locsAppBack/manage.py collectstatic --noinput
 python  /var/www/locsappWeb/locsAppBack/manage.py makemigrations
 python  /var/www/locsappWeb/locsAppBack/manage.py migrate
+cd /home/sylflo/project/epitech/locsapp/locsappWeb/locsAppFrontWeb/dev
+npm install
 /var/www/locsappWeb/locsAppFrontWeb/dev/launch_all_gulp.sh
 
