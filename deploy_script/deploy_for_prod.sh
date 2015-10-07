3475382d2f79e924258ffe0838d299145f1fcfdb
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-10-07 10:06:44 +0200

    deploy for prod

--- deploy_script/deploy_for_prod.sh
+++ deploy_script/deploy_for_prod.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-source /home/sylflo/.virtualenvs/locsAppBack/bin/activate
+cd /var/www/locsapp.sylflo.fr/locsappWeb
+git checkout .
+git pull
+
+source /var/lib/jenkins/.virtualenvs/locsApp/bin/activate   
 pip install -r /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/requirements.txt
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py collectstatic --noinput
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py makemigrations
