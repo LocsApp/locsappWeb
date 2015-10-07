dfcc0e7443121e5ede441fd60406bc7dfba1568b
caille_j <caille_j@epitech.eu>
2015-10-07 16:51:29 +0000

    updated unit test django

--- locsAppBack/API/tests.py
+++ locsAppBack/API/tests.py
@@ -9,7 +9,7 @@ class AccountTests(APITestCase):
 		Ensures we can create a new account object.
 		"""
 		url = "http://127.0.0.1:8000/api/v1/rest-auth/registration/"
-		data = {"username" : "test", "password1" : "Topkek1", "password2" : "Topkek1", "email" : "mamamia@gmail.co", "phone" : "0676283782", "is_active": "True", "last_name": "Kekman", "first_name": "Coco", "billing_address": "11 rue des keks", "birthdate": "1990-08-22 11:05:08", "living_address": "11 rue des keks"}
+		data = {"username" : "test", "password1" : "Topkek1", "password2" : "Topkek1", "email" : "mamamia@gmail.co", "phone" : "0676283782", "is_active": "True", "last_name": "Kekman", "first_name": "Coco", "billing_address": "11 rue des keks", "birthdate": "1990-08-22 11:05:08", "living_address": "11 rue des keks", "logo_url" : "/kek/"}
 		response = self.client.post(url, data, format='json')
 		try:
 			self.assertEqual(response.status_code, status.HTTP_201_CREATED)
