68169c02565e687a4151092bf3441283de560c7c
caille_j <caille_j@epitech.eu>
2015-10-08 15:30:22 +0000

    add of file class

---
+++ locsAppBack/API/APIrequest.py
@@ -0,0 +1,26 @@
+#Django imports
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+#JSON import
+import json
+
+class APIRequestMongo:
+
+	#creates a Create API POST
+	def forgeAPIrequestCreatePOST(self, request, fields, collection):
+		if (request.method == "POST"):
+			if (request.body):
+				answer = json.loads(request.body.decode('utf8'))
+				error_keys = {}
+				for key in fields:
+					if key not in answer:
+						error_keys[key] = "This key is required"
+				if (error_keys != {}):
+					return (JsonResponse(error_keys))
+				return(HttpResponse("200 OK"))
+			else:
+				return (HttpResponse("400 BAD REQUEST"))
+		else :
+			return (HttpResponse("405 METHOD NOT ALLOWED"))
