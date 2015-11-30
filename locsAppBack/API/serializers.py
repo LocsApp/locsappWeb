48e5be445bc50f172a8bb0344176a583c2e3ec31
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-30 18:11:43 +0800

    add UserPutSerializer

--- locsAppBack/API/serializers.py
+++ locsAppBack/API/serializers.py
@@ -8,3 +8,11 @@ class UserDetailsSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'first_name', 'last_name', 'birthdate', 'phone', 'living_address',
                   'registered_date', 'last_activity_date', 'billing_address', 'logo_url', "is_active", "role")
         read_only_fields = ('username', 'role')
+
+
+class UserPutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'birthdate', 'phone', 'living_address', 'billing_address', 'logo_url',
+                  'is_active', 'role')
+        read_only_fields = ('username', 'email', 'role', 'registered_date', 'last_activity_date',)
