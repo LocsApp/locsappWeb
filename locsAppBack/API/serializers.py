37e79d6dd5337ad13fa0320a8e55a0ec8d7bf0a1
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-30 18:01:36 +0800

    respect pep8 convention for serialzers.py

--- locsAppBack/API/serializers.py
+++ locsAppBack/API/serializers.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers, exceptions
 
+
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'birthdate', 'phone', 'living_address', 'registered_date', 'last_activity_date', 'billing_address', 'logo_url', "is_active", "role")
+        fields = ('username', 'email', 'first_name', 'last_name', 'birthdate', 'phone', 'living_address',
+                  'registered_date', 'last_activity_date', 'billing_address', 'logo_url', "is_active", "role")
         read_only_fields = ('username', 'role')
