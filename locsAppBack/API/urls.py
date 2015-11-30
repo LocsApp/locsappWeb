3e48b4488531da4be67ffaa4372a826ec78908e6
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-30 18:07:51 +0800

    views.py and urls.py respect covention pep8

--- locsAppBack/API/urls.py
+++ locsAppBack/API/urls.py
@@ -14,15 +14,16 @@ from allauth.account.views import confirm_email
 """
 
 article_patterns = [
-	url(r'^create/$', views.postNewArticle),
-	url(r'^delete/$', views.deleteArticle),
+    url(r'^create/$', views.postNewArticle),
+    url(r'^delete/$', views.deleteArticle),
 ]
 
 # General urls for the api
 api_patterns = [
-	url(r'^articles/', include(article_patterns)),
+    url(r'^articles/', include(article_patterns)),
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^email-sent/', RedirectView.as_view(url='http://127.0.0.1:8080/', permanent=False), name="account_email_verification_sent"),
+    url(r'^email-sent/', RedirectView.as_view(url='http://127.0.0.1:8080/', permanent=False),
+        name="account_email_verification_sent"),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls'), name='registrationUser'),
     url(r'^verify-email/(?P<key>\w+)/$', confirm_email, name="account_confirm_email"),
 ]
--- locsAppBack/API/views.py
+++ locsAppBack/API/views.py
@@ -1,4 +1,4 @@
-#Django imports
+# Django imports
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.http import JsonResponse
@@ -6,17 +6,17 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 
-#Pymongo imports
+# Pymongo imports
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-#JSON import
+# JSON import
 import json
 
-#APIrequestForgery class
+# APIrequestForgery class
 from .APIrequest import *
 
-#Instanciation of the APIRequest class
+# Instanciation of the APIRequest class
 APIrequests = APIRequestMongo()
 
 # Connects to the db and creates a MongoClient instance
@@ -24,42 +24,49 @@ mongodb_client = MongoClient('localhost', 27017)
 db_locsapp = mongodb_client['locsapp']
 
 # Template for the doc on the homepage of the API
+
+
 class docAPIView(TemplateView):
-	template_name = "API/homeDocAPI.html"
+    template_name = "API/homeDocAPI.html"
+
 
 """
-	GET END-POINTS
+    GET END-POINTS
 """
 
 """
-	POST END-POINTS
+    POST END-POINTS
 """
 
 """ Articles """
 
 # Creates a new article
+
+
 @csrf_exempt
 def postNewArticle(request):
-	fields_definition = {"name" : "text, 30",
-	 "tiny_logo_url" : "text, 255",
-	 "big_logo_url" : "text, 255",
-	 "description" : "text, 500",
-	 "id_author" : "integer",
-	 "date_created" : "date",
-	 "id_type" : "integer"}
+    fields_definition = \
+        {"name": "text, 30",
+         "tiny_logo_url": "text, 255",
+         "big_logo_url": "text, 255",
+         "description": "text, 500",
+         "id_author": "integer",
+         "date_created": "date",
+         "id_type": "integer"}
 
-	return APIrequests.forgeAPIrequestCreate("POST", request, fields_definition, db_locsapp["articles"])
+    return APIrequests.forgeAPIrequestCreate("POST", request, fields_definition, db_locsapp["articles"])
 
-
-"""
-	PUT END-POINTS
-"""
 
 """
-	DELETE END-POINTS
+    PUT END-POINTS
 """
+
+"""
+    DELETE END-POINTS
+"""
+
 
 @csrf_exempt
 def deleteArticle(request):
-	fields_definition = ["name", "id"]
-	return APIrequests.forgeAPIrequestDelete(request, fields_definition, db_locsapp["articles"])
+    fields_definition = ["name", "id"]
+    return APIrequests.forgeAPIrequestDelete(request, fields_definition, db_locsapp["articles"])
