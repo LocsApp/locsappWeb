3e2f02357520294ac857b74f1c274ee3b2796063
caille_j <caille_j@epitech.eu>
2015-11-30 18:24:38 +0000

    progress on editable field directive

--- locsAppFrontWeb/src/app/components/editableField/editableField.directive.js
+++ locsAppFrontWeb/src/app/components/editableField/editableField.directive.js
@@ -9,10 +9,26 @@
 	{
 		var directive = {};
 
-		directive.restrict = 'E';
+		directive.restrict = 'EA';
 		directive.templateUrl = 'app/components/editableField/editableField.tmpl.html';
+		directive.link =  function(scope) {
+			scope.changeEdition = function () {
+				scope.edition = !scope.edition;
+			};
+
+			scope.fallbackField = function () {
+				scope.field = scope.field_backup;
+			};
+
+			scope.updateBackupField = function (field) {
+				scope.field = field;
+				scope.field_backup = scope.field;
+			};
+		}
 		directive.scope = {
-			field : "=field"
+			field : "=field",
+			field_backup : "=field",
+			edition: "=edition"
 		};
 
 		return directive;
