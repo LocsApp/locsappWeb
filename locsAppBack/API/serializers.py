295961f78369a40826fab3b179194d5be6c49e2b
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-11-30 19:51:07 +0800

    add readonly fields for user

--- locsAppBack/API/serializers.py
+++ locsAppBack/API/serializers.py
@@ -7,12 +7,5 @@ class UserDetailsSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'birthdate', 'phone', 'living_address',
                   'registered_date', 'last_activity_date', 'billing_address', 'logo_url', "is_active", "role")
-        read_only_fields = ('username', 'role')
+        read_only_fields = ('username', 'role', 'email')
 
-
-class UserPutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ('first_name', 'last_name', 'birthdate', 'phone', 'living_address', 'billing_address', 'logo_url',
-                  'is_active', 'role')
-        read_only_fields = ('username', 'email', 'role', 'registered_date', 'last_activity_date',)
