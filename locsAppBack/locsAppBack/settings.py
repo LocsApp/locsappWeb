a6ef826e05e993e9dfa851f48b690d3c334b8bfd
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-01 18:00:39 +0800

    add new things

--- locsAppBack/locsAppBack/settings.py
+++ locsAppBack/locsAppBack/settings.py
@@ -181,7 +181,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "templates/api_doc/src")
+    #os.path.join(BASE_DIR, "templates/api_doc/src")
 )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
--- locsAppBack/tests/test_login.py
+++ locsAppBack/tests/test_login.py
@@ -15,15 +15,15 @@ class AccountRegisterTestCase(APITestCase):
         data = {"username": "test", "email": "toto@hotmail.fr", "password1": "toto42", "password2": "toto42"}
         self.client.post(self.register_url, data, format='json')
 
-    def test_create_account(self):
+    def test_account_not_valid(self):
         """
         Ensures we can create a new account object.
         """
         self.create_user()
-        #data = {"username": "test", "email": "toto@hotmail.fr", "password1": "toto42", "password2": "toto42"}
-        #response = self.client.post(self.url, data, format='json')
-        #self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        #self.assertTrue(response.data['key'])
+        data = {"username": "test", "password": "toto42"}
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Account.objects.count(), 1)
-        #self.assertEqual(Account.objects.get().username, 'test')
+        self.assertEqual(response.data, {'non_field_errors': ['E-mail is not verified.']})
+        self.assertEqual(Account.objects.get().username, 'test')
         #sself.assertEqual(Account.objects.get().email, 'toto@hotmail.fr')
