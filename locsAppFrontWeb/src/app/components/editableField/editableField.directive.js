f8beda4efa4f9b245e0f33c4fd771d4dbb79fea7
caille_j <caille_j@epitech.eu>
2015-12-01 12:56:19 +0000

    added scss for editablefield.scss, commented directive, removed garbage

--- locsAppFrontWeb/src/app/components/editableField/editableField.directive.js
+++ locsAppFrontWeb/src/app/components/editableField/editableField.directive.js
@@ -14,19 +14,23 @@
 		directive.templateUrl = 'app/components/editableField/editableField.tmpl.html';
 		directive.link =  function(scope) {
 
+			/*When scope.field is changed, calls the validation callback*/
 			scope.$watch('field', function(newValue) {
 				if (newValue)
 					scope.validation();
 			});
 
+			/*Sets the edition mode*/
 			scope.changeEdition = function () {
 				scope.edition = !scope.edition;
 			};
 
+			/*Sets scope.field back to its backup value*/
 			scope.fallbackField = function () {
 				scope.field = scope.field_backup;
 			};
-
+			
+			/*Sets scope.field to its new value as well as scope.field_backup*/
 			scope.updateBackupField = function (field) {
 				scope.field = field;
 				scope.field_backup = scope.field;
