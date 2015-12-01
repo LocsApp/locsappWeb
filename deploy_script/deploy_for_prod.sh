009c520e33b0517f7e7a63ef63df93dae560716d
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-01 18:02:45 +0800

    change scirpt for prod

--- deploy_script/deploy_for_prod.sh
+++ deploy_script/deploy_for_prod.sh
@@ -11,7 +11,7 @@ pip install -r /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/requirements.tx
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py collectstatic --noinput
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py makemigrations
 python  /var/www/locsapp.sylflo.fr/locsappWeb/locsAppBack/manage.py migrate
-cd /var/www/locsapp.sylflo.fr/locsappWeb/locsAppFrontWeb/dev/
+cd /var/www/locsapp.sylflo.fr/locsappWeb/locsAppFrontWeb/
 npm install
 bower install
 /var/www/locsapp.sylflo.fr/locsappWeb/locsAppFrontWeb/dev/launch_all_gulp.sh
