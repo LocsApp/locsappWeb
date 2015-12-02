d268232a98426b74d8d894c3879cffda27a51276
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-02 15:01:09 +0800

    remove code in serialzers.py usrs

--- locsAppBack/API/serializers.py
+++ locsAppBack/API/serializers.py
@@ -5,10 +5,8 @@ from rest_framework import serializers, exceptions
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        #secondary_emails = serializers.ListField(child=serializers.EmailField())
         scores = serializers.ListField(child=serializers.IntegerField(min_value=0, max_value=100))
         living_address = serializers.ListField(child=serializers.CharField())
-        #secondary_emails = serializers.ListField(child=serializers.EmailField())
 
         fields = ('scores', 'username', 'email', 'first_name', 'last_name', 'birthdate', 'phone', 'living_address',
                   'registered_date', 'last_activity_date', 'billing_address', 'logo_url', "is_active", "role")
