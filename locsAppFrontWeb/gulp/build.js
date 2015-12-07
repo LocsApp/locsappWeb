654b0d580aa4b4fdbb5cb20f472c7381433fe24f
caille_j <caille_j@epitech.eu>
2015-12-07 09:28:04 +0000

    added support for roboto slab font offline

--- locsAppFrontWeb/gulp/build.js
+++ locsAppFrontWeb/gulp/build.js
@@ -51,6 +51,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/material-design-iconfont/iconfont/', '../fonts/'))
+    .pipe($.replace('../../bower_components/roboto-slab-fontface-bower/fonts', '../fonts'))
     .pipe($.minifyCss({ processImport: false }))
     .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
