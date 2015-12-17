36268287a9b538c28053b0c52af87e100aa9c0e5
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-17 17:44:51 +0800

    add test for changing password

--- locsAppBack/tests/test_edit_profile.py
+++ locsAppBack/tests/test_edit_profile.py
@@ -11,6 +11,7 @@ class AccountEditProfileTestCase(APITestCase):
         self.url_user = "http://127.0.01:8000/api/v1/rest-auth/user/"
         self.url_register = "http://127.0.0.1:8000/api/v1/rest-auth/registration/"
         self.url_login = "http://localhost:8000/api/v1/rest-auth/login/"
+        self.url_change_password = "http://localhost:8000/api/v1/rest-auth/password/change/"
 
     def login_user(self):
         data = {"username": "locsapp", "password": "toto42"}
@@ -42,3 +43,28 @@ class AccountEditProfileTestCase(APITestCase):
         self.assertNotEqual(response.data['last_activity_date'], "2014/08/25")
 
         self.assertEqual(response.status_code, 200)
+
+    def test_change_password(self):
+        """
+        Ensure we can change the password and the user can't logged with the old password
+        """
+        key = self.login_user()
+        data = {"old_password": "toto42", "new_password1": "locsapp", "new_password2": "locsapp"}
+        header = {'HTTP_AUTHORIZATION': 'Token {}'.format(key)}
+        response = self.client.post(self.url_change_password, data, **header)
+        self.assertEqual({'success': 'New password has been saved.'}, response.data)
+        self.assertEqual(response.status_code, 200)
+
+        data = {"username": "locsapp", "password": "toto42"}
+        response = self.client.post(self.url_login, data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+        data = {"username": "locsapp", "password": "locsapp"}
+        response = self.client.post(self.url_login, data, format='json')
+        self.assertTrue(response.data['key'])
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
