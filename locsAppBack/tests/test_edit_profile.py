30efde9fe058d442ed03ee09b7f5c34744da5f7b
Sylvain Chateau <sylvain.chateau@epitech.eu>
2015-12-17 17:08:21 +0800

    add authentication token unit test and start edit profil unit test

--- locsAppBack/tests/test_edit_profile.py
+++ locsAppBack/tests/test_edit_profile.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from API.models import Account
 
 
@@ -10,15 +10,21 @@ class AccountEditProfileTestCase(APITestCase):
     def setUp(self):
         self.url_user = "http://127.0.01:8000/api/v1/rest-auth/user/"
         self.url_register = "http://127.0.0.1:8000/api/v1/rest-auth/registration/"
+        self.url_login = "http://localhost:8000/api/v1/rest-auth/login/"
 
-    def create_user(self):
-        data = {"username": "test", "email": "toto@hotmail.fr", "password1": "toto42", "password2": "toto42"}
-        self.client.post(self.url, data, format='json')
-
+    def login_user(self):
+        data = {"username": "locsapp", "password": "toto42"}
+        response = self.client.post(self.url_login, data, format='json')
+        return response.data['key']
 
     def test_edit_basic(self):
         """
         Ensure we can edit the basics information in the user profile
         """
-        data = {""}
-
+        key = self.login_user()
+        data = {"first_name": "LocsApp"}
+        header = {'HTTP_AUTHORIZATION': 'Token {}'.format(key)}
+        response = self.client.put(self.url_user, {"first_name": "toto"}, **header)
+        """client.credentials(HTTP_AUTHORIZATION='Token ' + key)
+        response = self.client.get(self.url_user, data, format='json')"""
+        print(response.data)
