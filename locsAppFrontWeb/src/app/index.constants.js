5aaff5fd99c17e9157ad7ca6e4f112eea03e5fae
caille_j <caille_j@epitech.eu>
2015-12-17 14:38:58 +0000

    align menu notif

--- locsAppFrontWeb/src/app/index.constants.js
+++ locsAppFrontWeb/src/app/index.constants.js
@@ -1,17 +1,8 @@
 (function() {
   'use strict';
 
-if (window.location.host.match(/locsapp\.sylflo\.fr/))
-{
-	angular
-	.module("LocsappServices")
-	.constant("URL_API", "http://locsapp.sylflo.fr/");
-}
-else
-{
 	angular
 	.module("LocsappServices")
 	.constant("URL_API", "http://127.0.0.1:8000/");	
-}
 
 })();
