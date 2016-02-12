1d244263e13a0ef117325673ec6324b986190170
caille_j <caille_j@epitech.eu>
2016-02-12 17:17:52 +0000

    rectified email success verif

--- locsAppFrontWeb/src/app/components/verifyEmail/verifyEmail.controller.js
+++ locsAppFrontWeb/src/app/components/verifyEmail/verifyEmail.controller.js
@@ -23,7 +23,7 @@
 
 		$http
 			.get(URL_API + 'api/v1/verify-email/'+ $stateParams.key + '/')
-			.then(vm.VerifyEmailFailure, vm.VerifyEmailSuccess);
+			.then(vm.VerifyEmailSuccess, vm.VerifyEmailFailure);
 	}
 
 })();
