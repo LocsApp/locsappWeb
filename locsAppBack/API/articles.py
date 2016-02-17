6eb2ce474f2bef90f19bd1b751177c410f4cb3b3
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-02-17 18:15:51 +0800

    add request.user.pk for id_author

--- locsAppBack/API/articles.py
+++ locsAppBack/API/articles.py
@@ -22,13 +22,13 @@ from bson import ObjectId
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
 def postNewArticle(request):
-    print("User = ", request.user.pk)
+    print("User = ", type(request.user.pk))
     fields_definition = \
         {"name": "text, 30",
          "tiny_logo_url": "text, 255 | http://127.0.0.1:8000/media/pictures/tiny_pictures/articles/dummy.png",
          "big_logo_url": "text, 255 | http://127.0.0.1:8000/media/pictures/big_pictures/articles/dummy.png",
          "description": "text, 500",
-         "id_author": "integer",
+         "id_author": "integer | " + request.user.pk,
          "date_created": "date_default",
          "id_type": "id",
          "comments": "array",
