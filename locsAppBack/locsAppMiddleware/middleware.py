2fc3e5ec0047e4e6b3310ec502eadde1d17d8bba
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-02-21 17:06:34 +0800

    remove function in middleware

--- locsAppBack/locsAppMiddleware/middleware.py
+++ locsAppBack/locsAppMiddleware/middleware.py
@@ -3,9 +3,6 @@ from django.http import JsonResponse
 
 class CheckUsernameExistMiddleware(object):
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        i = 0
-
     def process_response(self, request, response):
         if request.user.is_anonymous() == False and request.user.username == "":
             response.status_code = 499
