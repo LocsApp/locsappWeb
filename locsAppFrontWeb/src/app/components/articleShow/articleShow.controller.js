8cfbb7a65540b4bc4c55b19d06cc27fae47feb16
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-02-24 18:11:07 +0800

    add prev and next in controller for carousel

--- locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
+++ locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
@@ -32,21 +32,41 @@
 
       vm.currentIndex = 0;
       vm.slides = [
-        {image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg', description: 'Image 00'},
+        {
+          image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
+          description: 'Image 00'
+        },
         {image: 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516', description: 'Image 01'},
-        {image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020', description: 'Image 02'},
-         {image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg', description: 'Image 03'},
+        {
+          image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020',
+          description: 'Image 02'
+        },
+        {
+          image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
+          description: 'Image 03'
+        },
         {image: 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516', description: 'Image 04'},
-        {image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020', description: 'Image 05'},
+        {
+          image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020',
+          description: 'Image 05'
+        },
       ];
 
-      vm.setCurrentSlideIndex = function(index) {
+      vm.setCurrentSlideIndex = function (index) {
         vm.currentIndex = index;
       };
 
-      vm.isCurrentSlideIndex = function(index) {
+      vm.isCurrentSlideIndex = function (index) {
         return vm.currentIndex == index;
-      }
+      };
+
+      vm.prevSlide = function () {
+        vm.currentIndex = (vm.currentIndex < vm.slides.length - 1) ? ++vm.currentIndex : 0;
+      };
+
+      vm.nextSlide = function () {
+        vm.currentIndex = (vm.currentIndex > 0) ? --vm.currentIndex : vm.slides.length - 1;
+      };
 
 
     }
