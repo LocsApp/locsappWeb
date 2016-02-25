5df3da436068fd9cfc42f1aa7c48707d58d417f2
caille_j <caille_j@epitech.eu>
2016-02-25 17:52:02 +0000

    progress and fix update on create article

--- locsAppFrontWeb/src/app/components/articleCreate/articleCreate.controller.js
+++ locsAppFrontWeb/src/app/components/articleCreate/articleCreate.controller.js
@@ -10,7 +10,8 @@
 	var vm = this;
 
 	vm.value = 0;
-	vm.stepsComplete = [100, 0, 0, 0, 0, 0, 0];
 	vm.stepsNames = ["squared_one", "squared_two", "squared_three", "squared_four", "squared_five", "squared_six"];
+	vm.stepsComplete = [1, 0, 0, 0, 0, 0];
+	vm.progressBars = [0, 0, 0, 0, 0, 0]
   }
 })();
