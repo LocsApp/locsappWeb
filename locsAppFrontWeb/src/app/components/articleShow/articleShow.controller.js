3e125f7eeeb30cb1083f762f93453006e9744dd0
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-02-25 19:47:41 +0800

    add url pictures for test in success function getinfo

--- locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
+++ locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
@@ -12,8 +12,17 @@
 
     $log.log("In controller Article 42");
 
-     vm.GetInfoArticleSuccess = function (data) {
+    vm.GetInfoArticleSuccess = function (data) {
       $log.log("data = ", data);
+      vm.url_pictures = ['http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
+        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516',
+        'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020',
+        'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
+        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516',
+        'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020'
+      ];
+
+
     };
 
     vm.getInfoArticleFailure = function (data) {
@@ -25,8 +34,6 @@
       .get({id: "56cb41c0421aa91298799892"})
       .$promise
       .then(vm.GetInfoArticleSuccess, vm.getInfoArticleFailure);
-
-
 
 
     vm.clickImageGallery = function (event, index) {
