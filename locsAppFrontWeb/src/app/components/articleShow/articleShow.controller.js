50f01700ad22224324c8b4c29621585959b8f475
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-02-25 20:27:12 +0800

    remove comment in articleshow controller

--- locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
+++ locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
@@ -9,9 +9,6 @@
   /** @ngInject */
   function ArticleShowController($log, $mdDialog, $document, ArticleService) {
     var vm = this;
-  vm.url_pictures = [];
-    $log.log("In controller Article 42");
-
 
 
     vm.GetInfoArticleSuccess = function (data) {
@@ -24,7 +21,7 @@
         'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020'
       ];
 
-      $log.log("url picture", vm.url_pictures[0]);
+
 
 
     };
@@ -61,27 +58,6 @@
 
       vm.slides = slides;
       vm.currentIndex = index;
-      /*vm.slides = [
-        {
-          image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
-          description: 'Image 00'
-        },
-        {image: 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516', description: 'Image 01'},
-        {
-          image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020',
-          description: 'Image 02'
-        },
-        {
-          image: 'http://www.voguequeen.com/images/dresses/bridesmaids/20120921/fashion-chiffon-a-line-strapless-sleeveless-short-length-empire-bridesmaid-dress_120920005.jpg',
-          description: 'Image 03'
-        },
-        {image: 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=135603516', description: 'Image 04'},
-        {
-          image: 'http://cdn.shopify.com/s/files/1/0293/9277/products/Fashion_Nova_-_01-21-16-410_large.JPG?v=1453489020',
-          description: 'Image 05'
-        }
-      ];*/
-
 
 
       vm.setCurrentSlideIndex = function (index) {
