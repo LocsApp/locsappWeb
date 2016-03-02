eafc6dca90fbbb2bc5c8a0562c6f5e420b4a6200
caille_j <caille_j@epitech.eu>
2016-03-02 18:38:24 +0000

    finished first version of article create

--- locsAppFrontWeb/src/app/components/articleCreate/articleCreate.controller.js
+++ locsAppFrontWeb/src/app/components/articleCreate/articleCreate.controller.js
@@ -12,7 +12,7 @@
 	//steps vars
 	vm.value = 0;
 	vm.stepsNames = ["squared_one", "squared_two", "squared_three", "squared_four", "squared_five", "squared_six"];
-	vm.stepsComplete = [1, 1, 1, 1, 1, 1];
+	vm.stepsComplete = [1, 0, 0, 0, 0, 0];
 	vm.progressBars = [0, 0, 0, 0, 0, 0]
 	vm.stepFocus = 0;
 
