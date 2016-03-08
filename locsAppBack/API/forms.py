713adcb56d5ab502fece0ae97b7bb2fbefa38042
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-09 05:28:30 +0800

    forms remove unsued import

--- locsAppBack/API/forms.py
+++ locsAppBack/API/forms.py
@@ -1,11 +1,5 @@
 from django import forms
 from django.contrib.postgres.fields import ArrayField
-from django.utils.translation import ugettext_lazy as _
-
-from allauth.account.utils import (user_username, user_email,
-                                   user_field)
-from .adapter import get_adapter
-from django.conf import settings
 
 
 class SignupForm(forms.Form):
