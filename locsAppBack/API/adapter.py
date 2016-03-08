9fc322d0b957fbebc6566beda802cb97f522095b
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-09 05:27:15 +0800

    adapter remove unsued import

--- locsAppBack/API/adapter.py
+++ locsAppBack/API/adapter.py
@@ -1,25 +1,8 @@
 from __future__ import absolute_import
-from django.utils.translation import ugettext_lazy as _
-from django.core.urlresolvers import reverse
-from django.core.exceptions import ValidationError
-from allauth.utils import (import_attribute,
-                           email_address_exists,
-                           valid_email_or_none)
-from allauth.account.utils import user_email, user_username, user_field
-from allauth.account.models import EmailAddress
-from allauth.account.adapter import get_adapter as get_account_adapter
-from allauth.account import app_settings as account_settings
-from allauth.account.app_settings import EmailVerificationMethod
-from rest_framework.response import Response
-import json
-from django.http import HttpResponse
+from allauth.utils import import_attribute
 from allauth.socialaccount import app_settings
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
-from allauth.account.signals import user_signed_up, user_logged_in
-from django.dispatch import receiver
-from pprint import pprint
-from django.http import JsonResponse
 
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
