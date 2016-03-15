ca9709eb7bb5cc2e019c622488d949362fe29224
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-16 00:40:34 +0800

    good date when renting

--- locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
+++ locsAppFrontWeb/src/app/components/articleShow/articleShow.controller.js
@@ -114,8 +114,8 @@
       vm.brand = "Hermes";
       vm.state = "good";
       vm.description = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repell";
-      vm.start_availble = "start location";
-      vm.end_availble = "end location";
+      vm.start_availble = "3/15/2016";
+      vm.end_availble = "6/15/2016";
       vm.date_created = "Creation date";
       vm.date_modified = "Modified date";
       vm.long = "longitude";
@@ -127,6 +127,10 @@
       vm.id_renter = "two_ask";
 
       //request author profile to get his notation;
+
+      //Put the date min and max
+      vm.dateStart = new Date(vm.start_availble);
+      vm.dateEnd = new Date(vm.end_availble);
       // Add col and row for the grid gallery
       vm.obj_url_pictures = [];
       for (var i = 0; i < vm.url_pictures.length; i++) {
