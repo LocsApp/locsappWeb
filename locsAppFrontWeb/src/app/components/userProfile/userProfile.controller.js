65f303c579c99789d1058edc5e56e43745a17617
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-23 18:21:59 +0800

    userprofilecontroller add missing ;

--- locsAppFrontWeb/src/app/components/userProfile/userProfile.controller.js
+++ locsAppFrontWeb/src/app/components/userProfile/userProfile.controller.js
@@ -16,20 +16,20 @@
 	vm.GetInfosUserSuccess = function(data) {
 		$log.log(data);
 		vm.user = data;
-	}
+	};
 
 	/*Failure callback of profile_check*/
 	vm.GetInfosUserFailure = function(data) {
 		$log.log(data);
 		toastr.error("This is odd...", "Woops...");
-	}
+	};
 
 	/*Adds a new scope to share, and goes to profile_management*/
 	vm.goToParameters = function() {
 		ScopesService
 		.set("user_infos", vm.user);
 		$state.go("main.profile_management")
-	}
+	};
 
 	/*vm.user initializer*/
 	UsersService
