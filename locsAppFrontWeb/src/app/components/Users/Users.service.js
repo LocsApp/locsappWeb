02e5a42db4a62f73f1a4587716d9a39c03bedcc4
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-25 20:15:36 +0800

    new controller for paramters user profile

--- locsAppFrontWeb/src/app/components/Users/Users.service.js
+++ locsAppFrontWeb/src/app/components/Users/Users.service.js
@@ -13,6 +13,7 @@
       login: $resource(URL_API + 'api/v1/rest-auth/login/'),
       logout: $resource(URL_API + 'api/v1/rest-auth/logout/'),
       profile_check: $resource(URL_API + 'api/v1/rest-auth/user/', null, {'update': {method: 'PUT'}}),
+      modify_profile: $resource(URL_API + 'api/v1/rest-auth/user/', null, {'update': {method: 'PUT'}}),
       change_password: $resource(URL_API + 'api/v1/rest-auth/password/change/'),
       living_addresses: $resource(URL_API + 'api/v1/user/:id/living_addresses/', {id: "@user_id"}),
       living_addresses_delete: $resource(URL_API + 'api/v1/user/:id/living_addresses/delete/', {id: "@user_id"}),
