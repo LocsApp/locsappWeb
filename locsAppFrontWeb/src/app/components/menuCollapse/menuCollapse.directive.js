2cd9072c48539c14a0f798b4de5a58d192e6062d
oborotev <stormsnaps@gmail.com>
2016-04-06 18:39:37 +0000

    add of the directive

---
+++ locsAppFrontWeb/src/app/components/menuCollapse/menuCollapse.directive.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+	.module('LocsappDirectives')
+	.directive('menuCollapse', menuCollapse);
+
+	function menuCollapse()
+	{
+		var directive = {};
+
+		directive.restrict = 'EA';
+		directive.transclude =  true;
+		directive.templateUrl = 'app/components/menuCollapse/menuCollapse.tmpl.html';
+
+		directive.link =  function(scope) {
+			console.log(scope);
+
+			scope.isCollapsed = false;
+
+			scope.changeCollapse = function () {
+				scope.isCollapsed = !scope.isCollapsed;
+			};
+		}
+
+		directive.scope = {
+			title: "@title",
+			titleClass: "@titleClass",
+			bodyClass: "@bodyClass",
+			animation: "@bodyAnimation"
+		};
+
+		return directive;
+	}
+
+})();
