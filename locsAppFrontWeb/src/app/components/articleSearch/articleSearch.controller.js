c0aeb518373f3519bb30f36a56e9d9baeea690b0
oborotev <stormsnaps@gmail.com>
2016-04-06 18:39:20 +0000

    finished the side filters containers (fucking git corruption)

--- locsAppFrontWeb/src/app/components/articleSearch/articleSearch.controller.js
+++ locsAppFrontWeb/src/app/components/articleSearch/articleSearch.controller.js
@@ -6,9 +6,20 @@
 		.controller('ArticleSearchController', ArticleSearchController);
 
 	/** @ngInject */
-	function ArticleSearchController()
+	function ArticleSearchController(ArticleService, $log, toastr)
 	{
 		var vm = this;
+
+		/*Filters vars*/
+		vm.categories = null;
+		vm.subCategories = null;
+		vm.genders = null;
+		vm.sizes = null;
+		vm.clothe_colors = null;
+		vm.clothe_states = null;
+		vm.brands = [{_id: "56cb3ef2b2bc57ab2908e6b2", name: "Home made"}];
+		vm.payment_methods = null;
+		vm.filters = {};
 
 		/*Option vars*/
 		vm.sortingOptions = ["title", "price"];
@@ -26,6 +37,80 @@
 		{
 			vm.search._pagination.page_number += number;
 		}
+
+		//Static collection retrieval
+		vm.failedRetrieval = function (data) {
+			$log.log(data);
+			toastr.error("We couldn't retrieve some informations...", 'Woops...');
+		};
+
+
+		vm.getPaymentMethods = function (data) {
+			vm.payment_methods = data.payment_methods;
+		};
+
+		vm.getClotheStates = function (data) {
+			vm.clothe_states = data.clothe_states;
+
+			//And now we'll get payment
+			ArticleService
+			.getPaymentMethods
+			.get()
+			.$promise
+			.then(vm.getPaymentMethods, vm.failedRetrieval);
+		};
+
+		vm.getClotheColors = function (data) {
+			vm.clothe_colors = data.clothe_colors;
+			ArticleService
+			.getClotheStates
+			.get()
+			.$promise
+			.then(vm.getClotheStates, vm.failedRetrieval);
+		};
+
+		vm.getSizes = function (data) {
+			vm.sizes = data.sizes;
+			ArticleService
+			.getClotheColors
+			.get()
+			.$promise
+			.then(vm.getClotheColors, vm.failedRetrieval);
+		};
+
+		vm.getGenders = function (data) {
+			vm.genders = data.genders;
+			ArticleService
+			.getSizes
+			.get()
+			.$promise
+			.then(vm.getSizes, vm.failedRetrieval);
+		};
+
+		vm.getSubCategories = function (data) {
+		vm.subCategories = data.sub_categories;
+		ArticleService
+			.getGenders
+			.get()
+			.$promise
+			.then(vm.getGenders, vm.failedRetrieval);
+		};
+
+		vm.getCategories = function (data) {
+		vm.categories = data.base_categories;
+		ArticleService
+			.getSubCategories
+			.get()
+			.$promise
+			.then(vm.getSubCategories, vm.failedRetrieval);
+		};
+
+		ArticleService
+		.getCategories
+		.get()
+		.$promise
+		.then(vm.getCategories, vm.failedRetrieval);
+		//End of Static collection retrieval
 	}
 
 })();
