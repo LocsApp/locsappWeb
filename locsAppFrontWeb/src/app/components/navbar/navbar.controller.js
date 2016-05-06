f7f6c80fb9ba39168bdf84a1f3bd301f86bdfa13
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-05-07 03:32:56 +0800

    add dropdown menu

--- locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
+++ locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
@@ -1,58 +1,70 @@
-(function() {
+(function () {
   'use strict';
 
   angular
-	.module('LocsappControllers')
-	.controller('NavBarController', NavBarController);
+    .module('LocsappControllers')
+    .controller('NavBarController', NavBarController);
 
   /** @ngInject */
   function NavBarController(NotificationsService, $state, $log) {
-	var vm = this;
+    var vm = this;
 
-	/* Init vars */
-	vm.loader = NotificationsService.fetchingNotifications;
-	vm.page_notifications = 1;
+    /* Test */
+    this.openMenu = function($mdOpenMenu, ev) {
+      $mdOpenMenu(ev);
+    };
+    /* End test */
 
-	/*Notifications of the user*/
-	vm.notifications = NotificationsService.getNotifications;
-	
-	/*Opens the notifications menu*/
-	vm.openNotificationsMenu = function($mdOpenMenu, event) {
-		$mdOpenMenu(event);
-	};
 
-	/*Goes to the state parameter*/
-	vm.goToState = function(state_name) {
-		$state.go(state_name);
-	};
+    /* Init vars */
+    vm.loader = NotificationsService.fetchingNotifications;
+    vm.page_notifications = 1;
 
-	/*Sets the notification to read true*/
-	vm.notificationRead = function(notification) {
-		NotificationsService.notificationRead(notification);
-	};
+    /*Notifications of the user*/
+    vm.notifications = NotificationsService.getNotifications;
 
-	/*Sets all the notifications to read true*/
-	vm.notificationReadAll = function() {
-		NotificationsService.notificationReadAll("user");
-	};	
+    /*Opens the notifications menu*/
+    vm.openNotificationsMenu = function ($mdOpenMenu, event) {
+      $mdOpenMenu(event);
+    };
 
-	/*"Deletes" the notifications*/
-	vm.notificationDelete = function(notification) {
-		NotificationsService.notificationDelete(notification);
-	};
+    var originatorEv;
+    vm.openMenu = function ($mdOpenMenu, ev) {
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
 
-	/*"Deletes" all the notifications*/
-	vm.notificationDeleteAll = function() {
-		NotificationsService.notificationDeleteAll("user");
-	};
+    /*Goes to the state parameter*/
+    vm.goToState = function (state_name) {
+      $state.go(state_name);
+    };
 
-	/*Loads more notifications when scroll is at the bottom*/
-	vm.lazyLoadNotifications = function(scrollTop, scrollHeight) {
-		if (scrollTop == scrollHeight && !vm.loader())
-		{
-			NotificationsService.appendNewNotifications("user");
-			$log.log(scrollHeight);
-		}
-	}
+    /*Sets the notification to read true*/
+    vm.notificationRead = function (notification) {
+      NotificationsService.notificationRead(notification);
+    };
+
+    /*Sets all the notifications to read true*/
+    vm.notificationReadAll = function () {
+      NotificationsService.notificationReadAll("user");
+    };
+
+    /*"Deletes" the notifications*/
+    vm.notificationDelete = function (notification) {
+      NotificationsService.notificationDelete(notification);
+    };
+
+    /*"Deletes" all the notifications*/
+    vm.notificationDeleteAll = function () {
+      NotificationsService.notificationDeleteAll("user");
+    };
+
+    /*Loads more notifications when scroll is at the bottom*/
+    vm.lazyLoadNotifications = function (scrollTop, scrollHeight) {
+      if (scrollTop == scrollHeight && !vm.loader()) {
+        NotificationsService.appendNewNotifications("user");
+        $log.log(scrollHeight);
+      }
+    }
   }
 })();
