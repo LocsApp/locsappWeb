e299085da966f904e52e68bdcd8c2f222cee9675
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-05-07 04:48:11 +0800

    progress submenu

--- locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
+++ locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
@@ -11,9 +11,28 @@
 
     /* DropDownMenu */
     var originatorEv;
-    this.openMenuAccount = function ($mdOpenMenu, ev) {
-      $mdOpenMenu(ev);
+    this.openMenu = function($mdOpenMenu, ev) {
       originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
+    this.notificationsEnabled = true;
+    this.toggleNotifications = function() {
+      this.notificationsEnabled = !this.notificationsEnabled;
+    };
+    this.redial = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .targetEvent(originatorEv)
+          .clickOutsideToClose(true)
+          .parent('body')
+          .title('Suddenly, a redial')
+          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+          .ok('That was easy')
+      );
+      originatorEv = null;
+    };
+    this.checkVoicemail = function() {
+      // This never happens.
     };
     /* DropDownMenu */
 
