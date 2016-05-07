212889c39d512c2a5c331a35770ca8c42c4d6696
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-05-07 16:42:03 +0800

    save things

--- locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
+++ locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
@@ -4,32 +4,22 @@
   angular
     .module('LocsappControllers')
     .controller('NavBarController', NavBarController)
-    .controller('LeftCtrl', LeftCtrl)
-    .controller('RightCtrl', RightCtrl);
 
   /** @ngInject */
-  function LeftCtrl($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  }
+  function NavBarController(NotificationsService, $state, $log, $scope, $mdSidenav, $timeout) {
+    var vm = this;
 
-  /** @ngInject */
-  function RightCtrl($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
+    /* Side Nav Menu */
+    vm.icon = 'menu';
+
+    vm.close = function () {
       $mdSidenav('right').close()
         .then(function () {
           $log.debug("close RIGHT is done");
         });
     };
-  }
 
-  /** @ngInject */
-  function NavBarController(NotificationsService, $state, $log, $scope, $mdSidenav, $timeout) {
-    var vm = this;
+    /* End side nav menu
 
     /* DropDownMenu */
 
@@ -113,42 +103,16 @@
 
 
     /* Test Sidenav */
-    $scope.toggleLeft = buildDelayedToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function () {
+    vm.toggleRight = buildToggler('right');
+    vm.isOpenRight = function () {
       return $mdSidenav('right').isOpen();
     };
 
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-          args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function () {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
 
     /**
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
-    function buildDelayedToggler(navID) {
-      return debounce(function () {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
 
     function buildToggler(navID) {
       return function () {
