7161179150131b755a22a8f3c91b25d15b3aee81
oborotev <stormsnaps@gmail.com>
2016-06-15 22:55:55 +0700

    call to services and basis retrieval

--- locsAppFrontWeb/src/app/components/articleTimeline/articleTimeline.controller.js
+++ locsAppFrontWeb/src/app/components/articleTimeline/articleTimeline.controller.js
@@ -13,7 +13,37 @@
     vm.demands = true;
     vm.demandsAsRenting = true;
 
-    
+    vm.GetDemandsSuccess = function (data) {
+      $log.log(data);
+      vm.demands = data.article_demands;
+    };
+
+    vm.GetDemandsFailure = function (data) {
+      $log.log(data);
+    };
+
+    vm.GetDemandsRentingSuccess = function (data) {
+      $log.log("renting")
+      $log.log(data);
+      vm.demandsAsRenting = data.article_demands;
+    };
+
+    vm.GetDemandsRentingFailure = function (data) {
+      $log.log(data);
+    };
+
+    ArticleService
+    .currentTimelines
+    .get()
+    .$promise
+    .then(vm.GetDemandsSuccess, vm.GetDemandsFailure);
+
+    ArticleService
+    .currentTimelinesAsRenting
+    .get()
+    .$promise
+    .then(vm.GetDemandsRentingSuccess, vm.GetDemandsRentingFailure);
+
   }
 
  })();
