d97376b2d2de0a41797e1409f967b1b994189368
Sylvain Chateau <sylflo@Sylvains-MacBook-Pro.local>
2016-07-16 22:37:48 +0200

    acticvity date

--- locsAppFrontWeb/src/app/components/userProfile/userProfile.controller.js
+++ locsAppFrontWeb/src/app/components/userProfile/userProfile.controller.js
@@ -16,7 +16,8 @@
     vm.GetInfosUserSuccess = function (data) {
       $log.log(data);
       vm.user = data;
-      vm.user.registered_date = vm.user.registered_date.substring(0, 10);
+      vm.user.registered_date = vm.user.registered_date.substring(0, 10).replace(/-/g, '/');
+      vm.user.last_activity_date = vm.user.last_activity_date.split('T')[0].replace(/-/g, '/');
 
       //On affichaga de la plus recente a pla plus ancienne ca sera envoye par le backend
       vm.user.nb_mark_as_renter = 50;
@@ -42,14 +43,14 @@
           "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }//,
 
-     /*   {
-          "author_id": "dgdsgdshfs8787",
-          "author_username": "toto",
-          "title_article": "Veste rouge taille 42",
-          "id_article": "56cb41c0421aa91298799892",
-          "mark": [true, true, true, true, false],
-          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }*/
+        /*   {
+         "author_id": "dgdsgdshfs8787",
+         "author_username": "toto",
+         "title_article": "Veste rouge taille 42",
+         "id_article": "56cb41c0421aa91298799892",
+         "mark": [true, true, true, true, false],
+         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+         }*/
       ]
     };
 
