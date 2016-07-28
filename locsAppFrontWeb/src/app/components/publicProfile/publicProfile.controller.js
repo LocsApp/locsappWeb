40bdaa618f0715b80cb574909de9e34a50532b63
Sylvain Chateau <dev.chateau@gmail.com>
2016-07-28 15:37:31 +0200

    add notations to public user profile

--- locsAppFrontWeb/src/app/components/publicProfile/publicProfile.controller.js
+++ locsAppFrontWeb/src/app/components/publicProfile/publicProfile.controller.js
@@ -7,7 +7,7 @@
 
   /** @ngInject */
   function PublicProfileController($scope, $log, UsersService, ScopesService, $state, toastr, URL_API,
-  $stateParams) {
+                                   $stateParams) {
     var vm = this;
 
     /*vars initilization*/
@@ -16,10 +16,60 @@
     var i;
 
 
+    vm.firstFourNotationSuccess = function (data) {
+      $log.log("firstFourNotationSuccess ", data);
+
+      vm.public_user.renter_notation = data.renter_notation;
+      vm.public_user.client_notation = data.client_notation;
+      vm.public_user.nb_rotation_renter = data.nb_notation_renter;
+      vm.public_user.nb_notation_client = data.nb_notation_client;
+
+
+      for (i = 0; i < vm.public_user.renter_notation.length; i++) {
+        var score_renter = Math.round(vm.public_user.renter_notation[i].value);
+        var score_array_renter = [];
+
+        for (var j = 0; j < 5; j++) {
+          if (j < score_renter)
+            score_array_renter.push(true);
+          else
+            score_array_renter.push(false);
+        }
+        vm.public_user.renter_notation[i].score = score_array_renter;
+
+      }
+
+      for (i = 0; i < vm.public_user.client_notation.length; i++) {
+        var score_client = Math.round(vm.public_user.client_notation[i].value);
+        var score_array_client = [];
+
+        for (j = 0; j < 5; j++) {
+          if (j < score_client)
+            score_array_client.push(true);
+          else
+            score_array_client.push(false);
+        }
+        vm.public_user.client_notation[i].score = score_array_client;
+
+
+      }
+
+    };
+
+    vm.firstFourNotationFailure = function (data) {
+      $log.error("firstFourNotationFailure ", data);
+    };
+
     vm.GetArticleFromUserProfileSuccess = function (data) {
-      $log.log("GetArticleFromUserProfileSuccess ", data);
+      //$log.log("GetArticleFromUserProfileSuccess ", data);
       vm.public_user.nb_total_articles = data.nb_total_articles;
       vm.public_user.articles = data.articles;
+
+      UsersService
+        .firstFourNotation
+        .get({id: vm.public_user.id})
+        .$promise
+        .then(vm.firstFourNotationSuccess, vm.firstFourNotationSuccess)
 
     };
 
@@ -33,6 +83,29 @@
       vm.public_user.registered_date = vm.public_user.registered_date.substring(0, 10).replace(/-/g, '/');
       vm.public_user.last_activity_date = vm.public_user.last_activity_date.split('T')[0].replace(/-/g, '/');
 
+      vm.public_user.renter_score_array = undefined;
+      if (vm.public_user.renter_score != -1) {
+        var renter_score = Math.round(vm.public_user.renter_score);
+        vm.public_user.renter_score_array = [];
+        for (i = 0; i < 5; i++) {
+          if (i < renter_score)
+            vm.public_user.renter_score_array.push(true);
+          else
+            vm.public_user.renter_score_array.push(false);
+        }
+      }
+
+      vm.public_user.client_score_array = undefined;
+      if (vm.public_user.tenant_score != -1) {
+        var client_score = Math.round(vm.public_user.tenant_score);
+        vm.public_user.client_score_array = [];
+        for (i = 0; i < 5; i++) {
+          if (i < client_score)
+            vm.public_user.client_score_array.push(true);
+          else
+            vm.public_user.client_score_array.push(false);
+        }
+      }
 
       // We call the routes to get articles of the current user
       UsersService
@@ -47,6 +120,8 @@
     vm.GetPublicUserFailure = function (data) {
       $log.log(data);
       toastr.error("This is odd...", "Woops...");
+
+
     };
 
 
