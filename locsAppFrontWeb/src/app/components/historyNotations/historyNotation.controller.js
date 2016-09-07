148850219638d842f3be146f13af2158aa001dce
Sylvain Chateau <dev.chateau@gmail.com>
2016-09-07 21:09:48 +0800

    change name for variable pagination

--- locsAppFrontWeb/src/app/components/historyNotations/historyNotation.controller.js
+++ locsAppFrontWeb/src/app/components/historyNotations/historyNotation.controller.js
@@ -11,7 +11,7 @@
     var vm = this;
 
     vm.current_page = 1;
-    vm.page_size = 10;
+    vm.page_size = 10; // We use the same variable for the both
 
     vm.notationsClient = true;
     vm.notationsRenter = true;
@@ -27,8 +27,8 @@
     vm.GetNotationsClientSuccess = function (data) {
       $log.log("GetNotationsClientSuccess", data);
       vm.notationsClient = data.notations_as_client;
-      vm.nb_items = data.nb_page * vm.page_size;
-      vm.nb_page = data.nb_page;
+      vm.nb_items_client = data.nb_page * vm.page_size;
+      vm.nb_page_client = data.nb_page;
       for (var i =0; i < vm.notationsClient.length; i++)
       {
         vm.notationsClient[i].nb_stars = [0, 0, 0, 0, 0];
