6c772f395d68dc7d35af14ff598b872fad0565f0
Sylvain Chateau <dev.chateau@gmail.com>
2016-09-07 22:52:33 +0800

    working on history renter notation

--- locsAppFrontWeb/src/app/components/History/History.services.js
+++ locsAppFrontWeb/src/app/components/History/History.services.js
@@ -10,9 +10,9 @@
   function HistoryService($log, $resource, URL_API) {
 
     var service = {
-      getMarksForClients: $resource(URL_API + 'api/v1/history/notations-as-client:id_page/',
+      getMarksForClients: $resource(URL_API + 'api/v1/history/notations-as-client/:id_page/',
         {id_page: "id_page"}),
-      getMarksForRenters: $resource(URL_API + 'api/v1/history/notations-as-renter/',
+      getMarksForRenters: $resource(URL_API + 'api/v1/history/notations-as-renter/:id_page/',
         {id_page: "id_page"}),
 
       getArticlesForClients: $resource(URL_API + 'api/v1/history/articles-as-client/'),
