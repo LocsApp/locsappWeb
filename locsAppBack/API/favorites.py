9d6ee691e25b857b4d7299b24c6d0af757139bc9
Sylvain Chateau <dev.chateau@gmail.com>
2016-09-09 09:36:13 +0200

    favorite article use the pagination function

--- locsAppBack/API/favorites.py
+++ locsAppBack/API/favorites.py
@@ -11,6 +11,7 @@ from datetime import datetime
 import pytz
 import math
 from pymongo import ASCENDING, DESCENDING
+from .APIrequest import paginationAPI
 
 
 # Connects to the db and creates a MongoClient instance
@@ -132,6 +133,7 @@ def addFavoriteArticle(request):
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 def getFavoriteArticle(request, id_page):
+    """"
     item_in_a_page = 10
     id_page = int(id_page)
     nb_item = db_locsapp["favorite_article"].count({"id_user": request.user.pk})
@@ -146,11 +148,14 @@ def getFavoriteArticle(request, id_page):
         skip_page = 0
 
     for favorite_article in db_locsapp["favorite_article"].find(
-            {"id_user": request.user.pk}).sort("creation_date", DESCENDING).skip(
+            {"id_user": request.user.pk}).sort("date_issued", DESCENDING).skip(
                 (skip_page) * item_in_a_page).limit(item_in_a_page):
         favorite_article['_id'] = str(favorite_article['_id'])
         favorites_articles.append(favorite_article)
-
+    """
+    id_page = int(id_page)
+    field = {"id_user": request.user.pk}
+    nb_page, favorites_articles = paginationAPI(id_page, db_locsapp["favorite_article"], field)
     return JsonResponse(
         {"nb_page": nb_page, "favorite_article": favorites_articles})
 
