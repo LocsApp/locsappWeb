6468e18592786876ac4962fe134719cc13bf96e3
Sylvain Chateau <dev.chateau@gmail.com>
2016-09-17 15:31:40 +0200

    working on responsive menu

--- locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
+++ locsAppFrontWeb/src/app/components/navbar/navbar.controller.js
@@ -8,6 +8,28 @@
   /** @ngInject */
   function NavBarController(NotificationsService, $state, $log, $scope, $mdSidenav) {
     var vm = this;
+    vm.sections = [];
+     vm.sections.push({
+          name: 'Beers',
+          type: 'toggle',
+          pages: [{
+            name: 'IPAs',
+            type: 'link',
+            state: 'beers.ipas',
+            icon: 'fa fa-group'
+          }, {
+            name: 'Porters',
+            state: 'home.toollist',
+            type: 'link',
+            icon: 'fa fa-map-marker'
+          },
+          {
+            name: 'Wheat',
+            state: 'home.createTool',
+            type: 'link',
+            icon: 'fa fa-plus'
+          }]
+        });
 
     /* Side Nav Menu */
 
