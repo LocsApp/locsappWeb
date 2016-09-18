1f1679a1aa284fd9ce616b6c79c365df6816d836
Sylvain Chateau <dev.chateau@gmail.com>
2016-09-18 11:56:01 +0200

    trying to add module angular translate

--- locsAppFrontWeb/src/app/index.config.js
+++ locsAppFrontWeb/src/app/index.config.js
@@ -1,73 +1,93 @@
-(function() {
+(function () {
   'use strict';
 
-angular
-	.module('locsapp')
-	.factory('TokenAuthInterceptor', TokenAuthInterceptor);
+  angular
+    .module('locsapp')
+    .factory('TokenAuthInterceptor', TokenAuthInterceptor);
 
   TokenAuthInterceptor.$inject = ['$sessionStorage', '$localStorage', '$q'];
 
   function TokenAuthInterceptor($sessionStorage, $localStorage, $q) {
-	return {
-		request: function(config) {
-			config.headers = config.headers || {};
-			if ($localStorage.key)
-				config.headers.Authorization = 'Token ' + $localStorage.key;
-			else if ($sessionStorage.key)
-				config.headers.Authorization = 'Token ' + $sessionStorage.key;
-			return config || $q.when(config);
-		},
-		response: function(response) {
-			return response || $q.when(response);
-		}
-	};
+    return {
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($localStorage.key)
+          config.headers.Authorization = 'Token ' + $localStorage.key;
+        else if ($sessionStorage.key)
+          config.headers.Authorization = 'Token ' + $sessionStorage.key;
+        return config || $q.when(config);
+      },
+      response: function (response) {
+        return response || $q.when(response);
+      }
+    };
   }
 
   angular
-	.module('locsapp')
-	.config(config);
+    .module('locsapp')
+    .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig, $locationProvider, $httpProvider, $resourceProvider, cfpLoadingBarProvider, ezfbProvider) {
-	// Enable log
-	$logProvider.debugEnabled(true);
+  function config($logProvider, toastrConfig, $locationProvider, $httpProvider, $resourceProvider,
+                  cfpLoadingBarProvider, ezfbProvider) {
+    // Enable log
+    $logProvider.debugEnabled(true);
 
-	// Set options third-party lib
-	toastrConfig.allowHtml = true;
-	toastrConfig.timeOut = 3000;
-	toastrConfig.positionClass = 'toast-top-right';
+    // Set options third-party lib
+    toastrConfig.allowHtml = true;
+    toastrConfig.timeOut = 3000;
+    toastrConfig.positionClass = 'toast-top-right';
 
-	//Configuring of html5 routes
-	$locationProvider.html5Mode(true);
-	$locationProvider.hashPrefix('!');
+    //Configuring of html5 routes
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
 
-	//CSRF token sending automation
-	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
-	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    //CSRF token sending automation
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-	//Call of TokenAuthInterceptor for Authorization header sending automation
-	$httpProvider.interceptors.push('TokenAuthInterceptor');
+    //Call of TokenAuthInterceptor for Authorization header sending automation
+    $httpProvider.interceptors.push('TokenAuthInterceptor');
 
-	//Prevent resource from stripping the / on requests (Django stuff)
-	$resourceProvider.defaults.stripTrailingSlashes = false;
+    //Prevent resource from stripping the / on requests (Django stuff)
+    $resourceProvider.defaults.stripTrailingSlashes = false;
 
-	//Sets the angular-loading-bar settings
-	cfpLoadingBarProvider.selector = '#loading-bar-container';
-	cfpLoadingBarProvider.includeSpinner = false;
+    //Sets the angular-loading-bar settings
+    cfpLoadingBarProvider.selector = '#loading-bar-container';
+    cfpLoadingBarProvider.includeSpinner = false;
 
-  //Sets the Facebook local
-  ezfbProvider.setLocale('fr_FR');
+    //Sets the Facebook local
+    ezfbProvider.setLocale('fr_FR');
 
 
-   ezfbProvider.setInitParams({
-    appId: '1011661268854723',
+    ezfbProvider.setInitParams({
+      appId: '1011661268854723',
 
-    // Module default is `v2.4`.
-    // If you want to use Facebook platform `v2.3`, you'll have to add the following parameter.
-    // https://developers.facebook.com/docs/javascript/reference/FB.init
-    version: 'v2.5'
-  });
+      // Module default is `v2.4`.
+      // If you want to use Facebook platform `v2.3`, you'll have to add the following parameter.
+      // https://developers.facebook.com/docs/javascript/reference/FB.init
+      version: 'v2.5'
+    });
+
+
+    /* English translation */
+   /* $translateProvider.translations('en', {
+      'TITLE': 'Hello',
+      'FOO': 'This is a paragraph'
+    });*/
+
+    /* French translation */
+    /*$translateProvider.translations('fr', {
+      'TITLE': 'Hallo',
+      'FOO': 'Dies ist ein Absatz'
+    });*/
+
+    /* Default translation */
+    //$translateProvider.preferredLanguage('fr');
+
+
+
 
   }
 
